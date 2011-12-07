4278a1dd081799f2f1d669f9b96a78fd6fca9565
nebkat <nebkat@gmail.com>
2011-12-07 16:42:09 +0000

    Preferences: Show Page Indicator (Workspace)
    Change-Id: I564eca3da962f5af088e26b85d13dc3585780068

--- src/com/android/launcher2/Workspace.java
+++ src/com/android/launcher2/Workspace.java
@@ -242,6 +242,7 @@ public class Workspace extends SmoothPagedView
     // Preferences
     private boolean mShowSearchBar;
     private boolean mResizeAnyWidget;
+    private boolean mShowScrollingIndicator;
 
     /**
      * Used to inflate the Workspace from XML.
@@ -317,6 +318,7 @@ public class Workspace extends SmoothPagedView
         // Preferences
         mShowSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(context);
         mResizeAnyWidget = PreferencesProvider.Interface.Homescreen.getResizeAnyWidget(context);
+        mShowScrollingIndicator = PreferencesProvider.Interface.Homescreen.getShowScrollingIndicator(context);
 
         mLauncher = (Launcher) context;
         initWorkspace();
@@ -411,6 +413,10 @@ public class Workspace extends SmoothPagedView
                 paddingTop = (int)res.getDimension(R.dimen.qsb_bar_hidden_inset);
             }
             setPadding(0, paddingTop, getPaddingRight(), getPaddingBottom());
+        }
+
+        if (!mShowScrollingIndicator) {
+            disableScrollingIndicator();
         }
 
         mChangeStateAnimationListener = new AnimatorListenerAdapter() {
