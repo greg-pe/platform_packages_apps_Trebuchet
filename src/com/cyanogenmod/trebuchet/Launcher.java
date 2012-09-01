CyanogenMod/android_packages_apps_Trebuchet

Preferences: Hide search bar preference + others
- Restart Launcher on preferences changed
- Implement "getCurrentOrientation()"

Change-Id: I0728824ab994984c1dc798b61efa26b727ca3ea2

src/com/android/launcher2/Launcher.java
@@ -92,7 +92,7 @@ import android.widget.Toast;
 import com.android.common.Search;
 import com.android.launcher.R;
 import com.android.launcher2.DropTarget.DragObject;
-import com.android.launcher2.preference.Preferences;
+import com.android.launcher2.preference.*;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -246,6 +246,9 @@ public final class Launcher extends Activity
 
     private BubbleTextView mWaitingForResume;
 
+    // Preferences
+    private boolean mShowSearchBar;
+
     private Runnable mBuildLayersRunnable = new Runnable() {
         public void run() {
             if (mWorkspace != null) {
@@ -278,6 +281,9 @@ public final class Launcher extends Activity
         mAppWidgetManager = AppWidgetManager.getInstance(this);
         mAppWidgetHost = new LauncherAppWidgetHost(this, APPWIDGET_HOST_ID);
         mAppWidgetHost.startListening();
+
+        // Preferences
+        mShowSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(this);
 
         if (PROFILE_STARTUP) {
             android.os.Debug.startMethodTracing(
@@ -345,7 +351,7 @@ public final class Launcher extends Activity
         mSearchDropTargetBar.onSearchPackagesChanged(searchVisible, voiceVisible);
 
         // On large interfaces, we want the screen to auto-rotate based on the current orientation
-        if (LauncherApplication.isScreenLarge() || Build.TYPE.contentEquals("eng")) {
+        if (LauncherApplication.isScreenLarge()) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
     }
@@ -548,6 +554,10 @@ public final class Launcher extends Activity
     protected void onResume() {
         super.onResume();
         mPaused = false;
+        // Restart launcher when preferences are changed
+        if (preferencesChanged()) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
         if (mRestoring || mOnResumeNeedsLoad) {
             mWorkspaceLoading = true;
             mModel.startLoader(this, true);
@@ -745,6 +755,11 @@ public final class Launcher extends Activity
 
         // Get the search/delete bar
         mSearchDropTargetBar = (SearchDropTargetBar) mDragLayer.findViewById(R.id.qsb_bar);
+
+        // Hide the search divider if we are hiding search bar
+        if (!mShowSearchBar && getCurrentOrientation() == Configuration.ORIENTATION_LANDSCAPE) {
+            ((View) findViewById(R.id.qsb_divider)).setVisibility(View.GONE);
+        }
 
         // Setup AppsCustomize
         mAppsCustomizeTabHost = (AppsCustomizeTabHost)
@@ -2551,10 +2566,14 @@ public final class Launcher extends Activity
         }
     }
 
+    public int getCurrentOrientation() {
+        return getResources().getConfiguration().orientation;
+    }
+
     /** Maps the current orientation to an index for referencing orientation correct global icons */
     private int getCurrentOrientationIndexForGlobalIcons() {
         // default - 0, landscape - 1
-        switch (getResources().getConfiguration().orientation) {
+        switch (getCurrentOrientation()) {
         case Configuration.ORIENTATION_LANDSCAPE:
             return 1;
         default:
@@ -3144,7 +3163,6 @@ public final class Launcher extends Activity
     }
 
     /* Cling related */
-    private static final String PREFS_KEY = "com.android.launcher2.prefs";
     private boolean isClingsEnabled() {
         // TEMPORARY: DISABLE CLINGS ON LARGE UI
         if (LauncherApplication.isScreenLarge()) return false;
@@ -3183,7 +3201,7 @@ public final class Launcher extends Activity
                     cling.setVisibility(View.GONE);
                     cling.cleanup();
                     SharedPreferences prefs =
-                        getSharedPreferences("com.android.launcher2.prefs", Context.MODE_PRIVATE);
+                        getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = prefs.edit();
                     editor.putBoolean(flag, true);
                     editor.commit();
@@ -3207,7 +3225,7 @@ public final class Launcher extends Activity
     public void showFirstRunWorkspaceCling() {
         // Enable the clings only if they have not been dismissed before
         SharedPreferences prefs =
-            getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE);
+            getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
         if (isClingsEnabled() && !prefs.getBoolean(Cling.WORKSPACE_CLING_DISMISSED_KEY, false)) {
             initCling(R.id.workspace_cling, null, false, 0);
         } else {
@@ -3217,7 +3235,7 @@ public final class Launcher extends Activity
     public void showFirstRunAllAppsCling(int[] position) {
         // Enable the clings only if they have not been dismissed before
         SharedPreferences prefs =
-            getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE);
+            getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
         if (isClingsEnabled() && !prefs.getBoolean(Cling.ALLAPPS_CLING_DISMISSED_KEY, false)) {
             initCling(R.id.all_apps_cling, position, true, 0);
         } else {
@@ -3227,7 +3245,7 @@ public final class Launcher extends Activity
     public Cling showFirstRunFoldersCling() {
         // Enable the clings only if they have not been dismissed before
         SharedPreferences prefs =
-            getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE);
+            getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
         Cling cling = null;
         if (isClingsEnabled() && !prefs.getBoolean(Cling.FOLDER_CLING_DISMISSED_KEY, false)) {
             cling = initCling(R.id.folder_cling, null, true, 0);
@@ -3254,6 +3272,18 @@ public final class Launcher extends Activity
     public void dismissFolderCling(View v) {
         Cling cling = (Cling) findViewById(R.id.folder_cling);
         dismissCling(cling, Cling.FOLDER_CLING_DISMISSED_KEY, DISMISS_CLING_DURATION);
+    }
+
+    public boolean preferencesChanged() {
+        SharedPreferences prefs =
+            getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
+        boolean preferencesChanged = prefs.getBoolean(PreferencesProvider.PREFERENCES_CHANGED, false);
+        if (preferencesChanged) {
+            SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean(PreferencesProvider.PREFERENCES_CHANGED, false);
+                    editor.commit();
+        }
+        return preferencesChanged;
     }
 
     /**

src/com/android/launcher2/SearchDropTargetBar.java
@@ -29,6 +29,7 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 
 import com.android.launcher.R;
+import com.android.launcher2.preference.PreferencesProvider;
 
 /*
  * Ths bar will manage the transition between the QSB search bar and the delete drop
@@ -43,6 +44,8 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
     private AnimatorSet mDropTargetBarFadeOutAnim;
     private ObjectAnimator mQSBSearchBarFadeInAnim;
     private ObjectAnimator mQSBSearchBarFadeOutAnim;
+
+    private boolean mShowQSBSearchBar;
 
     private boolean mIsSearchBarHidden;
     private View mQSBSearchBar;
@@ -60,6 +63,8 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
 
     public SearchDropTargetBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+        mShowQSBSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(context);
     }
 
     public void setup(Launcher launcher, DragController dragController) {
@@ -88,6 +93,10 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
 
         boolean enableDropDownDropTargets =
             getResources().getBoolean(R.bool.config_useDropTargetDownTransition);
+
+        if (!mShowQSBSearchBar) {
+            mQSBSearchBar.setVisibility(View.GONE);
+        }
 
         // Create the various fade animations
         mDropTargetBar.setAlpha(0f);
@@ -153,20 +162,28 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
     public void showSearchBar(boolean animated) {
         cancelAnimations();
         if (animated) {
-            mQSBSearchBarFadeInAnim.start();
+            if (mShowQSBSearchBar) {
+                mQSBSearchBarFadeInAnim.start();
+            }
         } else {
-            mQSBSearchBar.setVisibility(View.VISIBLE);
-            mQSBSearchBar.setAlpha(1f);
+            if (mShowQSBSearchBar) {
+                mQSBSearchBar.setVisibility(View.VISIBLE);
+                mQSBSearchBar.setAlpha(1f);
+            }
         }
         mIsSearchBarHidden = false;
     }
     public void hideSearchBar(boolean animated) {
         cancelAnimations();
         if (animated) {
-            mQSBSearchBarFadeOutAnim.start();
+            if (mShowQSBSearchBar) {
+                mQSBSearchBarFadeOutAnim.start();
+            }
         } else {
-            mQSBSearchBar.setVisibility(View.GONE);
-            mQSBSearchBar.setAlpha(0f);
+            if (mShowQSBSearchBar) {
+                mQSBSearchBar.setVisibility(View.GONE);
+                mQSBSearchBar.setAlpha(0f);
+            }
         }
         mIsSearchBarHidden = true;
     }
@@ -191,7 +208,7 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
         mDropTargetBar.buildLayer();
         mDropTargetBarFadeOutAnim.cancel();
         mDropTargetBarFadeInAnim.start();
-        if (!mIsSearchBarHidden) {
+        if (!mIsSearchBarHidden && mShowQSBSearchBar) {
             mQSBSearchBarFadeInAnim.cancel();
             mQSBSearchBarFadeOutAnim.start();
         }
@@ -207,7 +224,7 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
             // Restore the QSB search bar, and animate out the drop target bar
             mDropTargetBarFadeInAnim.cancel();
             mDropTargetBarFadeOutAnim.start();
-            if (!mIsSearchBarHidden) {
+            if (!mIsSearchBarHidden && mShowQSBSearchBar) {
                 mQSBSearchBarFadeOutAnim.cancel();
                 mQSBSearchBarFadeInAnim.start();
             }

src/com/android/launcher2/Workspace.java
@@ -33,6 +33,7 @@ import android.content.ClipDescription;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
@@ -65,6 +66,7 @@ import android.widget.Toast;
 import com.android.launcher.R;
 import com.android.launcher2.FolderIcon.FolderRingAnimator;
 import com.android.launcher2.InstallWidgetReceiver.WidgetMimeTypeHandlerData;
+import com.android.launcher2.preference.PreferencesProvider;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -236,6 +238,9 @@ public class Workspace extends SmoothPagedView
     private float[] mNewRotationYs;
     private float mTransitionProgress;
 
+    // Preferences
+    private boolean mShowSearchBar;
+
     /**
      * Used to inflate the Workspace from XML.
      *
@@ -307,6 +312,9 @@ public class Workspace extends SmoothPagedView
         LauncherModel.updateWorkspaceLayoutCells(cellCountX, cellCountY);
         setHapticFeedbackEnabled(false);
 
+        // Preferences
+        mShowSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(context);
+
         mLauncher = (Launcher) context;
         initWorkspace();
 
@@ -349,11 +357,20 @@ public class Workspace extends SmoothPagedView
         setWillNotDraw(false);
         setChildrenDrawnWithCacheEnabled(true);
 
+        final Resources res = getResources();
+
         try {
-            final Resources res = getResources();
             mBackground = res.getDrawable(R.drawable.apps_customize_bg);
         } catch (Resources.NotFoundException e) {
             // In this case, we will skip drawing background protection
+        }
+
+        if (!mShowSearchBar) {
+            int paddingTop = 0;
+            if (mLauncher.getCurrentOrientation() == Configuration.ORIENTATION_PORTRAIT) {
+                paddingTop = (int)res.getDimension(R.dimen.qsb_bar_hidden_inset);
+            }
+            setPadding(0, paddingTop, getPaddingRight(), getPaddingBottom());
         }
 
         mChangeStateAnimationListener = new AnimatorListenerAdapter() {
@@ -3462,19 +3479,27 @@ public class Workspace extends SmoothPagedView
         final View qsbDivider = (ImageView) (parent.findViewById(R.id.qsb_divider));
         final View dockDivider = (ImageView) (parent.findViewById(R.id.dock_divider));
         if (qsbDivider != null && dockDivider != null) {
-            qsbDivider.setVisibility(View.VISIBLE);
+            if (mShowSearchBar) {
+                qsbDivider.setVisibility(View.VISIBLE);
+            }
             dockDivider.setVisibility(View.VISIBLE);
             if (mDividerAnimator != null) {
                 mDividerAnimator.cancel();
                 mDividerAnimator = null;
             }
             if (immediately) {
-                qsbDivider.setAlpha(1f);
+                if (mShowSearchBar) {
+                    qsbDivider.setAlpha(1f);
+                }
                 dockDivider.setAlpha(1f);
             } else {
                 mDividerAnimator = new AnimatorSet();
-                mDividerAnimator.playTogether(ObjectAnimator.ofFloat(qsbDivider, "alpha", 1f),
-                        ObjectAnimator.ofFloat(dockDivider, "alpha", 1f));
+                if (mShowSearchBar) {
+                    mDividerAnimator.playTogether(ObjectAnimator.ofFloat(qsbDivider, "alpha", 1f),
+                            ObjectAnimator.ofFloat(dockDivider, "alpha", 1f));
+                } else {
+                    mDividerAnimator.play(ObjectAnimator.ofFloat(dockDivider, "alpha", 1f));
+                }
                 mDividerAnimator.setDuration(sScrollIndicatorFadeInDuration);
                 mDividerAnimator.start();
             }
@@ -3491,14 +3516,20 @@ public class Workspace extends SmoothPagedView
                 mDividerAnimator = null;
             }
             if (immediately) {
-                qsbDivider.setVisibility(View.GONE);
+                if (mShowSearchBar) {
+                    qsbDivider.setVisibility(View.GONE);
+                    qsbDivider.setAlpha(0f);
+                }
                 dockDivider.setVisibility(View.GONE);
-                qsbDivider.setAlpha(0f);
                 dockDivider.setAlpha(0f);
             } else {
                 mDividerAnimator = new AnimatorSet();
-                mDividerAnimator.playTogether(ObjectAnimator.ofFloat(qsbDivider, "alpha", 0f),
-                        ObjectAnimator.ofFloat(dockDivider, "alpha", 0f));
+                if (mShowSearchBar) {
+                    mDividerAnimator.playTogether(ObjectAnimator.ofFloat(qsbDivider, "alpha", 0f),
+                            ObjectAnimator.ofFloat(dockDivider, "alpha", 0f));
+                } else {
+                    mDividerAnimator.play(ObjectAnimator.ofFloat(dockDivider, "alpha", 0f));
+                }
                 mDividerAnimator.addListener(new AnimatorListenerAdapter() {
                     private boolean cancelled = false;
                     @Override
@@ -3508,7 +3539,9 @@ public class Workspace extends SmoothPagedView
                     @Override
                     public void onAnimationEnd(android.animation.Animator animation) {
                         if (!cancelled) {
-                            qsbDivider.setVisibility(View.GONE);
+                            if (mShowSearchBar) {
+                                qsbDivider.setVisibility(View.GONE);
+                            }
                             dockDivider.setVisibility(View.GONE);
                         }
                     }

src/com/android/launcher2/preference/PreferencesProvider.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public final class PreferencesProvider {
-    private static final String PREFERENCES_FILE = "com.android.launcher2_preferences";
+    public static final String PREFERENCES_KEY = "com.android.launcher_preferences";
+
+    public static final String PREFERENCES_CHANGED = "preferences_changed";
+
     public static class Interface {
         public static class Homescreen {
-
+            public static boolean getShowSearchBar(Context context) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                return preferences.getBoolean("ui_homescreen_general_search", true);
+            }
         }
 
         public static class Drawer {

greg-pe/platform_packages_apps_Trebuchet

src/com/cyanogenmod/trebuchet/Launcher.java
@@ -95,7 +95,7 @@ import android.widget.Toast;
 import com.android.common.Search;
 import com.cyanogenmod.trebuchet.R;
 import com.cyanogenmod.trebuchet.DropTarget.DragObject;
-import com.cyanogenmod.trebuchet.preference.Preferences;
+import com.cyanogenmod.trebuchet.preference.*;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -290,6 +290,9 @@ public final class Launcher extends Activity
 
     private BubbleTextView mWaitingForResume;
 
+    // Preferences
+    private boolean mShowSearchBar;
+
     private Runnable mBuildLayersRunnable = new Runnable() {
         public void run() {
             if (mWorkspace != null) {
@@ -339,6 +342,9 @@ public final class Launcher extends Activity
         mAppWidgetManager = AppWidgetManager.getInstance(this);
         mAppWidgetHost = new LauncherAppWidgetHost(this, APPWIDGET_HOST_ID);
         mAppWidgetHost.startListening();
+
+        // Preferences
+        mShowSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(this);
 
         if (PROFILE_STARTUP) {
             android.os.Debug.startMethodTracing(
@@ -677,6 +683,10 @@ public final class Launcher extends Activity
         InstallShortcutReceiver.flushInstallQueue(this);
 
         mPaused = false;
+        // Restart launcher when preferences are changed
+        if (preferencesChanged()) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
         if (mRestoring || mOnResumeNeedsLoad) {
             mWorkspaceLoading = true;
             mModel.startLoader(true);
@@ -897,6 +907,11 @@ public final class Launcher extends Activity
 
         // Get the search/delete bar
         mSearchDropTargetBar = (SearchDropTargetBar) mDragLayer.findViewById(R.id.qsb_bar);
+
+        // Hide the search divider if we are hiding search bar
+        if (!mShowSearchBar && getCurrentOrientation() == Configuration.ORIENTATION_LANDSCAPE) {
+            ((View) findViewById(R.id.qsb_divider)).setVisibility(View.GONE);
+        }
 
         // Setup AppsCustomize
         mAppsCustomizeTabHost = (AppsCustomizeTabHost)
@@ -2837,10 +2852,14 @@ public final class Launcher extends Activity
         }
     }
 
+    public int getCurrentOrientation() {
+        return getResources().getConfiguration().orientation;
+    }
+
     /** Maps the current orientation to an index for referencing orientation correct global icons */
     private int getCurrentOrientationIndexForGlobalIcons() {
         // default - 0, landscape - 1
-        switch (getResources().getConfiguration().orientation) {
+        switch (getCurrentOrientation()) {
         case Configuration.ORIENTATION_LANDSCAPE:
             return 1;
         default:
@@ -3678,6 +3697,18 @@ public final class Launcher extends Activity
     public void dismissFolderCling(View v) {
         Cling cling = (Cling) findViewById(R.id.folder_cling);
         dismissCling(cling, Cling.FOLDER_CLING_DISMISSED_KEY, DISMISS_CLING_DURATION);
+    }
+
+    public boolean preferencesChanged() {
+        SharedPreferences prefs =
+            getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
+        boolean preferencesChanged = prefs.getBoolean(PreferencesProvider.PREFERENCES_CHANGED, false);
+        if (preferencesChanged) {
+            SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean(PreferencesProvider.PREFERENCES_CHANGED, false);
+                    editor.commit();
+        }
+        return preferencesChanged;
     }
 
     /**

src/com/cyanogenmod/trebuchet/SearchDropTargetBar.java
@@ -28,6 +28,7 @@ import android.view.animation.AccelerateInterpolator;
 import android.widget.FrameLayout;
 
 import com.cyanogenmod.trebuchet.R;
+import com.cyanogenmod.trebuchet.preference.PreferencesProvider;
 
 /*
  * Ths bar will manage the transition between the QSB search bar and the delete drop
@@ -42,6 +43,8 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
     private ObjectAnimator mQSBSearchBarAnim;
     private static final AccelerateInterpolator sAccelerateInterpolator =
             new AccelerateInterpolator();
+
+    private boolean mShowQSBSearchBar;
 
     private boolean mIsSearchBarHidden;
     private View mQSBSearchBar;
@@ -60,6 +63,8 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
 
     public SearchDropTargetBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+        mShowQSBSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(context);
     }
 
     public void setup(Launcher launcher, DragController dragController) {
@@ -108,6 +113,10 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
         mEnableDropDownDropTargets =
             getResources().getBoolean(R.bool.config_useDropTargetDownTransition);
 
+        if (!mShowQSBSearchBar) {
+            mQSBSearchBar.setVisibility(View.GONE);
+        }
+
         // Create the various fade animations
         if (mEnableDropDownDropTargets) {
             mDropTargetBar.setTranslationY(-mBarHeight);
@@ -136,30 +145,34 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
      */
     public void showSearchBar(boolean animated) {
         if (!mIsSearchBarHidden) return;
-        if (animated) {
-            prepareStartAnimation(mQSBSearchBar);
-            mQSBSearchBarAnim.reverse();
-        } else {
-            mQSBSearchBarAnim.cancel();
-            if (mEnableDropDownDropTargets) {
-                mQSBSearchBar.setTranslationY(0);
+        if (mShowQSBSearchBar) {
+            if (animated) {
+                prepareStartAnimation(mQSBSearchBar);
+                mQSBSearchBarAnim.reverse();
             } else {
-                mQSBSearchBar.setAlpha(1f);
+                mQSBSearchBarAnim.cancel();
+                if (mEnableDropDownDropTargets) {
+                    mQSBSearchBar.setTranslationY(0);
+                } else {
+                    mQSBSearchBar.setAlpha(1f);
+                }
             }
         }
         mIsSearchBarHidden = false;
     }
     public void hideSearchBar(boolean animated) {
         if (mIsSearchBarHidden) return;
-        if (animated) {
-            prepareStartAnimation(mQSBSearchBar);
-            mQSBSearchBarAnim.start();
-        } else {
-            mQSBSearchBarAnim.cancel();
-            if (mEnableDropDownDropTargets) {
-                mQSBSearchBar.setTranslationY(-mBarHeight);
+        if (mShowQSBSearchBar) {
+            if (animated) {
+                prepareStartAnimation(mQSBSearchBar);
+                mQSBSearchBarAnim.start();
             } else {
-                mQSBSearchBar.setAlpha(0f);
+                mQSBSearchBarAnim.cancel();
+                if (mEnableDropDownDropTargets) {
+                    mQSBSearchBar.setTranslationY(-mBarHeight);
+                } else {
+                    mQSBSearchBar.setAlpha(0f);
+                }
             }
         }
         mIsSearchBarHidden = true;
@@ -183,7 +196,7 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
         // Animate out the QSB search bar, and animate in the drop target bar
         prepareStartAnimation(mDropTargetBar);
         mDropTargetBarAnim.start();
-        if (!mIsSearchBarHidden) {
+        if (!mIsSearchBarHidden && mShowQSBSearchBar) {
             prepareStartAnimation(mQSBSearchBar);
             mQSBSearchBarAnim.start();
         }
@@ -199,7 +212,7 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
             // Restore the QSB search bar, and animate out the drop target bar
             prepareStartAnimation(mDropTargetBar);
             mDropTargetBarAnim.reverse();
-            if (!mIsSearchBarHidden) {
+            if (!mIsSearchBarHidden && mShowQSBSearchBar) {
                 prepareStartAnimation(mQSBSearchBar);
                 mQSBSearchBarAnim.reverse();
             }

src/com/cyanogenmod/trebuchet/Workspace.java
@@ -30,6 +30,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
@@ -58,6 +59,7 @@ import android.widget.TextView;
 import com.cyanogenmod.trebuchet.R;
 import com.cyanogenmod.trebuchet.FolderIcon.FolderRingAnimator;
 import com.cyanogenmod.trebuchet.LauncherSettings.Favorites;
+import com.cyanogenmod.trebuchet.preference.PreferencesProvider;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -256,6 +258,9 @@ public class Workspace extends SmoothPagedView
     private float[] mNewRotationYs;
     private float mTransitionProgress;
 
+    // Preferences
+    private boolean mShowSearchBar;
+
     /**
      * Used to inflate the Workspace from XML.
      *
@@ -333,6 +338,9 @@ public class Workspace extends SmoothPagedView
 
         LauncherModel.updateWorkspaceLayoutCells(cellCountX, cellCountY);
         setHapticFeedbackEnabled(false);
+
+        // Preferences
+        mShowSearchBar = PreferencesProvider.Interface.Homescreen.getShowSearchBar(context);
 
         mLauncher = (Launcher) context;
         initWorkspace();
@@ -421,6 +429,14 @@ public class Workspace extends SmoothPagedView
             mBackground = res.getDrawable(R.drawable.apps_customize_bg);
         } catch (Resources.NotFoundException e) {
             // In this case, we will skip drawing background protection
+        }
+
+        if (!mShowSearchBar) {
+            int paddingTop = 0;
+            if (mLauncher.getCurrentOrientation() == Configuration.ORIENTATION_PORTRAIT) {
+                paddingTop = (int)res.getDimension(R.dimen.qsb_bar_hidden_inset);
+            }
+            setPadding(0, paddingTop, getPaddingRight(), getPaddingBottom());
         }
 
         mWallpaperOffset = new WallpaperOffsetInterpolator();
@@ -3774,8 +3790,10 @@ public class Workspace extends SmoothPagedView
         final View scrollIndicator = getScrollingIndicator();
 
         cancelScrollingIndicatorAnimations();
-        if (qsbDivider != null) qsbDivider.setAlpha(reducedFade);
-        if (dockDivider != null) dockDivider.setAlpha(reducedFade);
+        if (mShowSearchBar) {
+            if (qsbDivider != null) qsbDivider.setAlpha(reducedFade);
+            if (dockDivider != null) dockDivider.setAlpha(reducedFade);
+        }
         scrollIndicator.setAlpha(1 - fade);
     }
 }

src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public final class PreferencesProvider {
-    private static final String PREFERENCES_FILE = "com.cyanogenmod.trebuchet_preferences";
+    public static final String PREFERENCES_KEY = "com.cyanogenmod.trebuchet_preferences";
+
+    public static final String PREFERENCES_CHANGED = "preferences_changed";
     public static class Interface {
         public static class Homescreen {
-
+            public static boolean getShowSearchBar(Context context) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                return preferences.getBoolean("ui_homescreen_general_search", true);
+            }
         }
 
         public static class Drawer {